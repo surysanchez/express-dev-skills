2ecb4f1a2685e224039fdd237831503d859b64bf
surysanchez <surysanchezsales@gmail.com>
2023-03-28 21:56:13 -0400

    add array

---
+++ models/skill.js
@@ -0,0 +1,16 @@
+var skills = [
+    {id: 123, skill: HTML, done: true},
+    {id: 234, skill: CSS, done: true},
+    {id: 345, skill: JavaScript, done: true},
+    {id: 456, skill: Python, done: false}
+]
+
+module.exports = {
+    getSkills
+
+}
+
+
+function getSkills() {
+    return skills
+}
