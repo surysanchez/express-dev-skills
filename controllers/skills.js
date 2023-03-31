f78364fff488c9b9ab6484be4f8fb32f403be28b
surysanchez <surysanchezsales@gmail.com>
2023-03-30 21:25:58 -0400

    add header and footer

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,20 +1,20 @@
-// controllers/todos.js
-const Skill = require('../models/skill');
+//controllers/todos.js
+const Skill = require("../models/skill");
 
 module.exports = {
-   index,
-   show
+  index,
+  show,
 };
 function index(req, res) {
-    res.render('skills/index', {
-      skills: Skill.getSkills()
-    });
-  }
- 
-  function show(req, res) {
-    res.render('skills/show', { 
-      skill: Skill.showSkills(req.params.id),
-    });
-  }
+  res.render("skills/index", {
+    skills: Skill.getSkills(),
+    title: "All Skills",
+  });
+}
 
-
+function show(req, res) {
+  res.render("skills/show", {
+    skill: Skill.showSkills(req.params.id),
+    title: "To-Learn Details",
+  });
+}
